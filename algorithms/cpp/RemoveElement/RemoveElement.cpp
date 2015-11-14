f6bf9e9fc818314b96f2d86b1c04f323da7b309b
ksong <ksong@freewheel.tv>
2015-11-14 22:56:45 +0800

    27, remove element

---
+++ algorithms/cpp/RemoveElement/RemoveElement.cpp
@@ -0,0 +1,27 @@
+// source: https://leetcode.com/problems/remove-element/
+
+/**
+ *
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+ *
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ *
+ **/
+
+class Solution {
+public:
+	int removeElement(vector<int>& nums, int val) {
+		int length = 0;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (nums[i] == val)
+				continue;
+			else
+			{
+				nums[length] = nums[i];
+				length++;
+			}
+		}
+		return length;
+	}
+};
