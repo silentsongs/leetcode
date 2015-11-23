606ff713a64a722cf30cf431f786f5e7b931c884
ksong <ksong@freewheel.tv>
2015-11-23 22:01:52 +0800

    258, add digits

---
+++ algorithms/cpp/AddDigits/AddDigits.cpp
@@ -0,0 +1,23 @@
+// source: https://leetcode.com/problems/add-digits/
+
+/**
+ *
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+ *
+ * For example:
+ *
+ * Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ *
+ * Follow up:
+ * Could you do it without any loop/recursion in O(1) runtime?
+ *
+ **/
+
+class Solution {
+public:
+	int addDigits(int num) {
+		if (num == 0) return 0;
+		int x = num % 9;
+		return x == 0 ? 9 : x;
+	}
+};
